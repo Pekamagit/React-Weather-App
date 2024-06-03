5c48b4d0a4d7ce053049b6fafaaf9d73c4df93c0
Pekamagit <160333069+Pekamagit@users.noreply.github.com>
2024-06-03 20:37:07 +0100

    Add files via upload

---
+++ src/components/search/index.jsx
@@ -0,0 +1,18 @@
+
+
+export default function search({search, setSearch, handleSearch} ) {
+
+    return <div className="search-engine">
+        <input 
+            type="text"
+            placeholder="Enter City Name"
+            name="search"
+            value={search}
+            onChange={(Event)=> setSearch(Event.target.value) }
+        />
+        <button onClick={handleSearch}>
+        Search
+        </button>
+    </div>
+    
+}
